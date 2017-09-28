25ee7a72f4c2aef8239929776f18283d286775ec
Alvaro Felipe <alvaro@ed.team>
2017-09-28 09:45:06 -0500

    primer commit, estructura inicial

---
+++ gulpfile.babel.js
@@ -0,0 +1,65 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import plumber from 'gulp-plumber';
+import pug from 'gulp-pug';
+import browserSync from 'browser-sync';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import cssnano from 'cssnano';
+
+const server = browserSync.create();
+
+let postcssPlugins = [
+  //autoprefixer({browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'}),
+  cssnano({
+    core: false,
+    autoprefixer: {
+      add: true,
+      browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'
+    }
+  })
+];
+
+let sassOptions = {
+  outputStyle: 'expanded'
+};
+
+gulp.task('styles', () =>
+  gulp.src('./dev/scss/styles.scss')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(sass(sassOptions))
+    .pipe(postcss(postcssPlugins))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('./public/css'))
+    .pipe(server.stream({match: '**/*.css'}))
+);
+
+gulp.task('pug', () =>
+  gulp.src('./dev/pug/pages/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./public'))
+);
+
+gulp.task('scripts', () =>
+  gulp.src('./dev/js/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./public/js'))
+);
+
+
+gulp.task('default', () => {
+  server.init({
+    server: {
+      baseDir: './public'
+    },
+  });
+
+  gulp.watch('./dev/scss/**/*.scss', ['styles']);
+  gulp.watch('./dev/js/**/*.js', ['scripts', server.reload]);
+  gulp.watch('./dev/pug/**/*.pug', ['pug', server.reload]);
+});
