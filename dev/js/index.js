e8a25d1b7528b21755934c5f28cec53d87c6779a
Alvaro Felipe <alvaro@ed.team>
2017-09-28 20:34:06 -0500

    Se agregan dos módulos ES6 de ejemplo

---
+++ dev/js/index.js
@@ -0,0 +1,4 @@
+import {saludo, despedida} from "./modules/example";
+
+saludo();
+despedida();
---
+++ dev/js/modules/example.js
@@ -0,0 +1,10 @@
+// Este es un ejemplo de como exportar funciones desde un archivo
+// En index.js se importan estas funciones
+
+export const saludo = () => {
+  console.log('Hola mundo')
+};
+
+export const despedida = () => {
+  console.log('Adiós mundo')
+};
