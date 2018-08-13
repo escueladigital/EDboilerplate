f41d64c53ff431d76b3e792711e4ec978a043f2a
Alvaro Felipe <alvaro@ed.team>
2018-08-13 09:00:04 -0500

    Se actualizan paquetes de npm y gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,29 @@
-import gulp from 'gulp';
-import plumber from 'gulp-plumber';
-import pug from 'gulp-pug';
-import browserSync from 'browser-sync';
-import sass from 'gulp-sass';
-import postcss from 'gulp-postcss';
-import cssnano from 'cssnano';
-import watch from 'gulp-watch';
-import browserify from 'browserify';
-import babelify from 'babelify';
-import source from 'vinyl-source-stream';
-import sourcemaps from 'gulp-sourcemaps';
-import buffer from 'vinyl-buffer';
+import gulp from 'gulp'
+import plumber from 'gulp-plumber'
+import pug from 'gulp-pug'
+import browserSync from 'browser-sync'
+import sass from 'gulp-sass'
+import postcss from 'gulp-postcss'
+import cssnano from 'cssnano'
+import watch from 'gulp-watch'
+import browserify from 'browserify'
+import babelify from 'babelify'
+import source from 'vinyl-source-stream'
+import sourcemaps from 'gulp-sourcemaps'
+import buffer from 'vinyl-buffer'
+import minify from 'gulp-minify'
 
 const server = browserSync.create();
 
+const production = true
+const env = production ? 'prod' : 'dev'
+const srcJs = production ? '.js' : '-min.js'
+const minJs = production ? '-min.js' : '.js'
+
 const postcssPlugins = [
   cssnano({
-    core: false,
+    core: true,
+    zindex: false,
     autoprefixer: {
       add: true,
       browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'
@@ -24,51 +31,61 @@ const postcssPlugins = [
   })
 ];
 
-const sassOptions = {
+gulp.task('serve', function() {
+  server.init({
+    server: {
+      baseDir: './public'
+    }
+  });
+
+  watch('./themes/custom/escueladigital/scss/**/*.scss', () => gulp.start('styles'));
+  watch('./themes/custom/escueladigital/babel/**/*.js', () => gulp.start('scripts',server.reload) );
+});
+
+const sassOptions = env == 'dev' ? {
+  includePaths: ['node_modules'],
+  sourceComments: true,
   outputStyle: 'expanded'
-};
+} : {
+  includePaths: ['node_modules'],
+}
 
-gulp.task('styles', () =>
-  gulp.src('./dev/scss/styles.scss')
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(plumber())
-    .pipe(sass(sassOptions))
-    .pipe(postcss(postcssPlugins))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./public/css'))
-    .pipe(server.stream({match: '**/*.css'}))
-);
-
-gulp.task('pug', () =>
-  gulp.src('./dev/pug/pages/*.pug')
-    .pipe(plumber())
-    .pipe(pug())
-    .pipe(gulp.dest('./public'))
-);
+gulp.task('styles', () => {
+  return env == 'dev' 
+    ? gulp.src('./dev/scss/styles.scss')
+      .pipe(plumber())
+      .pipe(sass(sassOptions))
+      .pipe(gulp.dest('./public/css/'))
+      .pipe(server.stream({match: '**/*.css'}))
+    : gulp.src('./dev/scss/styles.scss')
+      .pipe(plumber())
+      .pipe(sass(sassOptions))
+      .pipe(postcss(postcssPlugins))
+      .pipe(gulp.dest('./public/css/'))
+      .pipe(server.stream({match: '**/*.css'}))
+});
 
 gulp.task('scripts', () =>
   browserify('./dev/js/index.js')
-    .transform(babelify)
+    .transform(babelify, {
+      global: true // permite importar desde afuera (como node_modules)
+    })
     .bundle()
     .on('error', function(err){
       console.error(err);
       this.emit('end')
     })
-    .pipe(source('scripts.js'))
+    .pipe(source('ed.js'))
     .pipe(buffer())
+    .pipe(minify({
+      ext: {
+        src: srcJs,
+        min: minJs
+      }
+    }))
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./public/js'))
 );
 
-gulp.task('default', () => {
-  server.init({
-    server: {
-      baseDir: './public'
-    },
-  });
-
-  watch('./dev/scss/**/*.scss', () => gulp.start('styles'));
-  watch('./dev/js/**/*.js', () => gulp.start('scripts',server.reload) );
-  watch('./dev/pug/**/*.pug', () => gulp.start('pug', server.reload) );
-});
+gulp.task('default', ['serve', 'styles', 'scripts']);
